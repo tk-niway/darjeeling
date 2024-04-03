ad1dc5167dd9bcd2f7fc22028e1cd43abbac28b3
tk-niway <urban.hang274@gmail.com>
2024-04-04 00:33:34 +0900

    installed & seted up fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule); # Express.js
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  console.log('🚀App is running on port:', port);
+
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();

0df33902dc134df281596d83306b0bba35f8c552
tk-niway <urban.hang274@gmail.com>
2024-04-06 01:48:44 +0900

    installed graphql packages

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,25 +1,75 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    es6: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint: ecommended',
+    'plugin: typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: [
+    'import',
+    'sort-keys-fix',
+    'typescript-sort-keys',
+    'unused-imports',
+  ],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/no-duplicates': 'error',
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'typescript-sort-keys/interface': 'error',
+    'unused-imports/no-unused-imports': 'error',
   },
 };
+
+// default settings for Nestjs
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: 'tsconfig.json',
+//     tsconfigRootDir: __dirname,
+//     sourceType: 'module',
+//   },
+//   plugins: ['@typescript-eslint/eslint-plugin'],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   root: true,
+//   env: {
+//     node: true,
+//     jest: true,
+//   },
+//   ignorePatterns: ['.eslintrc.js'],
+//   rules: {
+//     '@typescript-eslint/interface-name-prefix': 'off',
+//     '@typescript-eslint/explicit-function-return-type': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+// };

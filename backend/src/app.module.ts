b59b79f785262574e91171d2fe47de3b4b65c2f2
tk-niway <urban.hang274@gmail.com>
2024-04-06 09:42:50 +0900

    set up Mercurius

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { join } from 'path';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: join(process.cwd(), 'src/generated/schema.gql'),
+      sortSchema: true,
+    }),
+    RecipesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

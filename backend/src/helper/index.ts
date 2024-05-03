7a634ce4b819e121923c58cd590e3e2904ec63e6
tk-niway <urban.hang274@gmail.com>
2024-05-04 01:06:48 +0900

    refactored

--- backend/src/helper/index.ts
+++ backend/src/helper/index.ts
@@ -2,18 +2,19 @@ import { ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { Request, Response } from 'express';
 
+export function convertContext(context: ExecutionContext) {
+  return GqlExecutionContext.create(context).getContext();
+}
+
 export function convertReq(context: ExecutionContext) {
-  const gqlContext = GqlExecutionContext.create(context);
-
-  const ctx = gqlContext.getContext();
-
-  return ctx.req || context.switchToHttp().getRequest<Request>();
+  return (
+    convertContext(context).req || context.switchToHttp().getRequest<Request>()
+  );
 }
 
 export function convertRes(context: ExecutionContext) {
-  const gqlContext = GqlExecutionContext.create(context);
-
-  const ctx = gqlContext.getContext();
-
-  return ctx.req || context.switchToHttp().getResponse<Response>();
+  return (
+    convertContext(context).req ||
+    context.switchToHttp().getResponse<Response>()
+  );
 }

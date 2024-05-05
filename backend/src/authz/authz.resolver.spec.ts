95919e39ced373b53959191c88aa122b5e14c331
tk-niway <urban.hang274@gmail.com>
2024-05-05 10:02:15 +0900

    implemented test cases for authzResolver

--- backend/src/authz/authz.resolver.spec.ts
+++ backend/src/authz/authz.resolver.spec.ts
@@ -27,4 +27,56 @@ describe('AuthzResolver', () => {
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  describe('signup', () => {
+    it('should return a user after signup', async () => {
+      const token = 'token';
+      jest.spyOn(resolver, 'signup').mockResolvedValue({
+        id: '1',
+        name: 'User 1',
+        auth0Id: 'auth0Id',
+        email: '',
+        isActive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      const user = await resolver.signup(token);
+
+      expect(user).toBeDefined();
+      expect(user.id).toBeDefined();
+      expect(user.name).toBeDefined();
+      expect(user.auth0Id).toBeDefined();
+      expect(user.email).toBeDefined();
+      expect(user.isActive).toBeDefined();
+      expect(user.createdAt).toBeDefined();
+      expect(user.updatedAt).toBeDefined();
+    });
+  });
+
+  describe('signin', () => {
+    it('should return a user after signin', async () => {
+      const token = 'token';
+      jest.spyOn(resolver, 'signin').mockResolvedValue({
+        id: '1',
+        name: 'User 1',
+        auth0Id: 'auth0Id',
+        email: '',
+        isActive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      const user = await resolver.signin(token);
+
+      expect(user).toBeDefined();
+      expect(user.id).toBeDefined();
+      expect(user.name).toBeDefined();
+      expect(user.auth0Id).toBeDefined();
+      expect(user.email).toBeDefined();
+      expect(user.isActive).toBeDefined();
+      expect(user.createdAt).toBeDefined();
+      expect(user.updatedAt).toBeDefined();
+    });
+  });
 });

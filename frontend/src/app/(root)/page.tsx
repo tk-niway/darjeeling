f5c01d57ad8ad8c85e8ced0dc7431af4a55d580b
tk-niway <urban.hang274@gmail.com>
2024-06-15 19:50:55 +0900

    feat: Add video player component for streaming videos

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+"use client";
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+
+const VideoPlayer = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const handlePlayerError = (error: any) => {
+    console.error("Player Error:", error);
+  };
+
   return (
     <>
-      <h1>Welcome to Darjeeling</h1>
+      <h1>Video Player</h1>
+      {isMounted && (
+        <ReactPlayer
+          url="http://localhost:3333/files/test_vio/test_vio.m3u8"
+          controls
+          autoplay
+          muted
+          onError={handlePlayerError}
+        />
+      )}
     </>
   );
-}
+};
+
+export default VideoPlayer;

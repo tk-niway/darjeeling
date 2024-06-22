ac64d1231de33939c00b6856632464e0067a687e
tk-niway <urban.hang274@gmail.com>
2024-06-22 13:26:56 +0900

    chore: Remove unused PrismaService import and update AuthzModule and UsersModule providers

--- backend/src/authz/authz.module.ts
+++ backend/src/authz/authz.module.ts
@@ -2,19 +2,12 @@ import { AuthzService } from 'src/authz/authz.service';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/authz/jwt.strategy';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthzResolver } from 'src/authz/authz.resolver';
 import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
-  providers: [
-    AuthzService,
-    JwtStrategy,
-    PrismaService,
-    AuthzResolver,
-    UsersService,
-  ],
+  providers: [AuthzService, JwtStrategy, AuthzResolver, UsersService],
   exports: [PassportModule],
 })
 export class AuthzModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { UsersResolver } from 'src/users/users.resolver';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthzModule } from 'src/authz/authz.module';
 
 @Module({
   imports: [AuthzModule],
-  providers: [UsersService, UsersResolver, PrismaService],
+  providers: [UsersService, UsersResolver],
 })
 export class UsersModule {}

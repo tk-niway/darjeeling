b236f1b2a4a4c981cd42156110bbe107acd33aca
tk-niway <urban.hang274@gmail.com>
2024-08-05 23:26:59 +0900

    feat: Update VideoPlayer component to use usePublicVideosQuery
    This commit updates the VideoPlayer component to use the usePublicVideosQuery hook from the generated hooks file. It fetches a list of public videos with the specified visibility and isActive filters. The loading state is handled, and the fetched data is logged to the console. This change enhances the VideoPlayer component by using the generated hook for more efficient and optimized data fetching.

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,82 +1,31 @@
 "use client";
 import { useAuthUser } from "@/app/_providers/authUserProvider";
-import React, { useState, useEffect } from "react";
-import ReactPlayer from "react-player";
+import { usePublicVideosQuery } from "@/generated/hooks";
+import { VideoVisibility } from "@/types";
+import React, { useState, useEffect, use } from "react";
 
 const VideoPlayer = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const [videoUrl, setVideoUrl] = useState("");
-  const { token, isLoading } = useAuthUser();
+  const { data, loading, error } = usePublicVideosQuery({
+    variables: {
+      take: 30,
+      where: {
+        isActive: { equals: true },
+        visibility: { equals: VideoVisibility.Public },
+      },
+    },
+  });
 
   useEffect(() => {
-    setIsMounted(true);
-  }, []);
+    if (loading) return;
 
-  useEffect(() => {
-    const fetchVideoUrl = async () => {
-      console.log("Fetching video URL", token);
-      try {
-        const response = await fetch(
-          `http://localhost:3333/videos/clxq6ebyn0000uqjfbbsrfsw6`,
-          {
-            headers: {
-              Authorization: token ? `Bearer ${token}` : "",
-            },
-          }
-        );
-
-        console.log({ response });
-        if (!response.ok) throw new Error("Video not found");
-
-        const filePath = await response.text();
-
-        console.log({ filePath });
-        setVideoUrl(filePath);
-      } catch (error) {
-        console.error("Error fetching video URL:", error);
-      }
-    };
-
-    if (!isLoading) fetchVideoUrl();
-  }, [token, isLoading]);
-
-  const handlePlayerError = (error: any) => {
-    console.error("Player Error:", error);
-  };
+    if (!loading && data) {
+      console.log({ data });
+    }
+  }, [loading]);
 
   return (
     <>
-      <h1>Video Player</h1>
-      {videoUrl && (
-        <ReactPlayer
-          url={videoUrl}
-          controls
-          playsinline
-          onError={(error) => console.error("Player Error:", error)}
-          config={{
-            file: {
-              hlsOptions: {
-                forceHLS: true,
-                debug: false,
-                xhrSetup: (xhr: XMLHttpRequest) => {
-                  if (token)
-                    xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-                },
-              },
-            },
-          }}
-        />
-      )}
-      {/* {isMounted && (
-        <ReactPlayer
-          url="http://localhost:3333/files/videoplayback/videoplayback.m3u8"
-          controls
-          autoplay
-          muted
-          playsinline
-          onError={handlePlayerError}
-        />
-      )} */}
+      <h1>Videos</h1>
     </>
   );
 };

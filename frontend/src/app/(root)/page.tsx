434554e3b645da919e44e1f6104e956fb67d43ad
tk-niway <urban.hang274@gmail.com>
2024-08-06 23:44:21 +0900

    feat: Update VideoPlayer component to use usePublicVideosQuery
    This commit enhances the VideoPlayer component by updating it to use the usePublicVideosQuery hook from the generated hooks file. It fetches a list of public videos with the specified visibility and isActive filters, handles the loading state, and logs the fetched data to the console. This change improves the efficiency and optimization of data fetching in the VideoPlayer component.

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useAuthUser } from "@/app/_providers/authUserProvider";
 import { usePublicVideosQuery } from "@/generated/hooks";
-import { VideoVisibility } from "@/types";
+import { VideoVisibility, PublicVideosQuery, VideoModel } from "@/types";
+import Link from "next/link";
 import React, { useState, useEffect, use } from "react";
 
 const VideoPlayer = () => {
@@ -11,21 +12,50 @@ const VideoPlayer = () => {
       where: {
         isActive: { equals: true },
         visibility: { equals: VideoVisibility.Public },
+        url: { not: null },
       },
     },
   });
+  const [videos, setVideos] = useState<PublicVideosQuery["videos"]["nodes"]>(
+    [] as PublicVideosQuery["videos"]["nodes"]
+  );
+  const [pageInfo, setPageInfo] = useState<
+    PublicVideosQuery["videos"]["pageInfo"]
+  >({} as PublicVideosQuery["videos"]["pageInfo"]);
+  const [totalCount, setTotalCount] =
+    useState<PublicVideosQuery["videos"]["totalCount"]>(0);
 
   useEffect(() => {
     if (loading) return;
 
     if (!loading && data) {
       console.log({ data });
+      setVideos(data.videos.nodes);
+      setPageInfo(data.videos.pageInfo);
+      setTotalCount(data.videos.totalCount);
     }
-  }, [loading]);
+  }, [loading, data]);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
     <>
       <h1>Videos</h1>
+      <div>
+        {videos && videos.length === 0 && <p>No videos found</p>}
+        {videos &&
+          videos.map((video) => (
+            <div key={video.id}>
+              <Link href={`/videos/${video.id}`}>
+                <h2>{video.title}</h2>
+              </Link>
+              <p>{video.description}</p>
+              <p>{video.playCount}</p>
+              <p>{video.url}</p>
+              <p>{video.duration}</p>
+            </div>
+          ))}
+      </div>
     </>
   );
 };
